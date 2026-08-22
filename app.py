47fc5b6b545a16e17a41b9efe74c43368fa1ee28
smit naik <monyanaik150506@gmail.com>
2026-08-23 00:58:33 +0530

    Fix formatting in print statement for Jenkins message

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ print("revised file")
 print("Hari Hari bol")
 print("Hare Krishna")
 
-print("Tutorial 4: app.py modified - Jenkins Poll SCM detected the change")
+print("Tutorial 4: app.py modified -Jenkins Poll SCM detected the change")
 
 
